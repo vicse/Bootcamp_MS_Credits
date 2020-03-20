7dc404cbdc8e8443cf1e72f66696c97dcd38aaf9
vicse <vicse.ore@tecsup.edu.pe>
2020-03-20 02:01:20 -0500

    Project Structure completed

---
+++ src/main/java/com/vos/bootcamp/ms_credits/MsCreditsApplication.java
@@ -0,0 +1,13 @@
+package com.vos.bootcamp.ms_credits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCreditsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MsCreditsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/vos/bootcamp/ms_credits/MsCreditsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vos.bootcamp.ms_credits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCreditsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
